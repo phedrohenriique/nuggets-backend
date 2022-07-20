6cd45f37a1196dcaed9ada7b2691fa54f1330aae
Pedro Henrique <phd19994@gmail.com>
2022-07-19 23:30:40 -0300

    TSL for development added

--- nuggets/app.py
+++ nuggets/app.py
@@ -9,6 +9,9 @@ app = sn.Sanic('nuggets')
 app.blueprint(rt.routes)
 
 app.config.CORS_ORIGINS = "*"  ## need to be setted to send response CORS allowed headers
+app.config.RESPONSE_TIMEOUT = 1
+app.config.REQUEST_TIMEOUT = 1
+app.config.LOCAL_CERT_CREATOR = "trustme"
 
 ## app object will be running the routes = sn.Blueprint.group() where all the routes will be stated
 
